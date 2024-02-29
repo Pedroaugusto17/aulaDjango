33dee0b05b5140104684227e935b655c503f6065
Pedroaugusto17 <pedroaufi@hotmail.com>
2024-02-28 22:12:09 -0300

    primeira aula django

---
+++ produto/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+
+class Home(View):
+    template_name = "produto/index.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(request,self.template_name)
